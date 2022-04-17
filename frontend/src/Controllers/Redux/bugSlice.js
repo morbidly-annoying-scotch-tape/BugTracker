b8d8e604ec2118b203a5e923ada2d135d996a141
ShadowSec <shadowsec.security@protonmail.com>
2022-04-17 13:45:28 -0400

    redux slices

---
+++ frontend/src/Controllers/Redux/bugSlice.js
@@ -0,0 +1,20 @@
+import {} from '@reduxjs/toolkit'
+
+const slice = createSlice({
+    name:"bug",
+    initialState:[],
+    reducers:{
+        getBugs:(state)=>{
+
+        },
+        createBugs:(state,actions)=>{
+
+        },
+        updateBugs:(state,actions)=>{
+
+        },
+        markComplete:(state,action)=> {
+            
+        }
+    }
+})
